3bb6349c67169e46c3120921d219a8462e997fe7
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-08-11 16:09:35 +0300

    Изменил расширение файлов с js на jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './assets/styles/typography.css';
+
+function App() {
+  return (
+    <div className="text-style-raleway-39-px-regular">
+      Hello!
+    </div>
+  );
+}
+
+export default App;
