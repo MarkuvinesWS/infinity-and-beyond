97a2588d2cef26c40ad616f368d89ef3011a02ac
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-08-12 17:32:04 +0300

    Добавил моковые данные

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import './App.css';
 import './assets/styles/typography.css';
 
 function App() {
+  const [activeTariff, setActiveTariff] = useState(0);
+  const list = [
+    ' + Enjoy on all your devices',
+    ' + Watch on two screens',
+    ' + Watch anywhere you want',
+    ' + Resolution adapted to your device',
+  ];
+  const tariffs = [
+    { id: 1, name: 'monthly' },
+    { id: 2, name: '6 months' },
+    { id: 3, name: 'Yearly' },
+  ];
   return (
     <div className="text-style-raleway-39-px-regular">
       Hello!
