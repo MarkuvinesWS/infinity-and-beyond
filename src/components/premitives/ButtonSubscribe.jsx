c5f28ba5d80a0dca7497b4af7b67f98979415b92
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-08-12 09:04:17 +0300

    Добавил кнопку подписки

---
+++ src/components/premitives/ButtonSubscribe.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function ButtonSubscribe({ text }) {
+  return (
+    <StyledButton
+      className="text-style-raleway-title-01"
+      type="button"
+    >
+      {text}
+    </StyledButton>
+  );
+}
+
+ButtonSubscribe.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default ButtonSubscribe;
+
+const StyledButton = styled.div`
+  padding: 14px 36px;
+  width: fit-content;
+  height: fit-content;
+  background: var(--color-black-10);
+  border-radius: 63px;
+  color:var(--color-roxo-100);
+  cursor: pointer;
+  transition: all .3s ease-out;
+  &:hover {
+    transition: all .3s ease-out;
+    background-color: var(--color-roxo-20);
+  }
+  &:active {
+    transition-delay: 2ms;
+    background: var(--color-roxo-70);
+    color: var(--color-black-10);
+  }
+`;
