89ce712e13c9b6c48105f45833cf073e4ae2d2b1
Жиленко Евгений <e.zhilenko@worksolutions.ru>
2022-09-29 14:01:36 +0300

    Добавил моковые данные. Добавил моковые профили

---
+++ src/mock/profiles/index.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, name: 'Mike T', src: 'image/profiles/profile_1.jpg' },
+  { id: 2, name: 'Katy Allan', src: 'image/profiles/profile_2.jpg' },
+  { id: 3, name: 'William S', src: 'image/profiles/profile_3.jpg' },
+  { id: 4, name: 'Fabiana J', src: 'image/profiles/profile_4.jpg' },
+];
